0a32bd56e667e90a313c2e566a5965218d576229
AliveKiwi <prateekjais.dev@gmail.com>
2024-06-03 08:51:26 +0530

    95 navigation: Added CategoriesScreen to NavigationContainer>Stack.Navigator>Stack.Screen in App.js

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import CategoriesScreen from './screens/CategoriesScreen';
 
+const Stack = createStackNavigator();
+console.log(Stack);
 export default function App() {
   return (
     <>
       {/* 94 added StatusBar */}
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      {/* 95 added Navigation */}
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
