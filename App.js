abcf64c6645690065da8fb7fbf463e09699fee7b
AliveKiwi <prateekjais.dev@gmail.com>
2024-06-04 07:17:50 +0530

    102 navigation: added screenOptions={} to Navigator and options={} to screen for styling header

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
@@ -15,8 +15,22 @@ export default function App() {
       <StatusBar style="dark" />
       {/* 95 added Navigation */}
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        {/* 102 Added screenOptions to add header styles */}
+        <Stack.Navigator
+          screenOptions={{
+            headerTintColor: '#ffffff',
+            headerStyle: { backgroundColor: '#351401' },
+            contentStyle: { backgroundColor: '#3f2f25' },
+          }}
+        >
+          {/* 102 added options to style header*/}
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: 'All Categories',
+            }}
+          />
           {/* 96 added MealsOverviewScreen*/}
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
         </Stack.Navigator>
