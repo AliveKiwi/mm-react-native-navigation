10f6d93c01db8ca435560086f53f5964bd93d510
AliveKiwi <prateekjais.dev@gmail.com>
2024-06-07 05:26:40 +0530

    106 style: Styled the MealDetailScreen, added ScrollView and CSS cascading by passing textStyle to MealDetails UC

--- component/MealDetails.js
+++ component/MealDetails.js
@@ -2,18 +2,28 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
-export default function MealDetails({ duration, complexity, affordability }) {
+export default function MealDetails({
+  duration,
+  complexity,
+  affordability,
+  style,
+  textStyle,
+}) {
   return (
-    <View style={styles.details}>
-      <Text style={styles.detailItem}>{duration}m</Text>
-      <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-      <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+    <View style={[styles.details, style]}>
+      <Text style={[styles.detailItem, textStyle]}>{duration}m</Text>
+      <Text style={[styles.detailItem, textStyle]}>
+        {complexity.toUpperCase()}
+      </Text>
+      <Text style={[styles.detailItem, textStyle]}>
+        {affordability.toUpperCase()}
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // 105
+  // 105 display items in a row, and make them center
   details: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -21,7 +31,7 @@ const styles = StyleSheet.create({
     padding: 8,
   },
 
-  // 105
+  // 105 set the size and margin of text items
   detailItem: {
     marginHorizontal: 4,
     fontSize: 12,
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,35 +1,62 @@
 // 104 created
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
 import MealDetails from '../component/MealDetails';
+import Subtitle from '../component/MealDetail/Subtitle';
+import List from '../component/MealDetail/List';
 
 export default function MealDetailScreen({ route }) {
   const mealId = route.params.mealId;
-
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
-    <View>
-      <Image source={{ uri: selectedMeal.imageUrl }} />
-      <Text>{selectedMeal.title}</Text>
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
       {/* 105 */}
       <MealDetails
         duration={selectedMeal.duration}
         complexity={selectedMeal.complexity}
         affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
       />
-      <Text>Ingredients</Text>
-      {selectedMeal.ingredients.map((ingredient) => (
-        <Text key={ingredient}>{ingredient}</Text>
-      ))}
-      <Text>Steps</Text>
-      {selectedMeal.steps.map((step) => (
-        <Text key={step}>{step}</Text>
-      ))}
-    </View>
+      {/* 106 centers the Subtitle and List */}
+      <View style={styles.listOuterContainer}>
+        {/* 106 set Subtitle and List to expand 80%*/}
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List items={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List items={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: { marginBottom: 32 },
+  image: {
+    width: '100%',
+    height: 350,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    margin: 8,
+    textAlign: 'center',
+    color: 'white',
+  },
+  // 106 set color of text in MealDetails
+  detailText: { color: 'white' },
+  // 106 centers the Subtitle and List
+  listOuterContainer: {
+    alignItems: 'center',
+  },
+  // 106 set Subtitle and List to expand 80%
+  listContainer: {
+    width: '80%',
+  },
+});
