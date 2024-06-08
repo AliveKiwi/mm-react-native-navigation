c6f77f4d5dbd1e22f82df951cd9d8eb88cbdefae
AliveKiwi <prateekjais.dev@gmail.com>
2024-06-08 20:38:15 +0530

    119 component: FavoritesScreen now render list of favorite meals

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,12 +1,39 @@
+import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+
+import MealsList from '../component/MealsList/MealsList';
+import { FavoritesContext } from '../store/context/favorites-context';
+import { MEALS } from '../data/dummy-data';
 
 export default function FavoritesScreen() {
-  return (
-    <View>
-      <Text>FavoritesScreen</Text>
-    </View>
+  // 119 favoriteMealsCtx contains only ids and add/remove ids method
+  const favoriteMealsCtx = useContext(FavoritesContext);
+
+  // 119 get favoriteMeals data using the ids
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsCtx.ids.includes(meal.id)
   );
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
+
+  return <MealsList items={favoriteMeals} />;
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
